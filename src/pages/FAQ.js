57b76d506d62496e1ca8d83280140de6be1855f7
Okubo <aspastrana990@gmail.com>
2020-09-27 20:00:14 -0300

    first commit

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import Layout from "./components/layout"
+import Layout from "./components/Layout"
 import useFrecuente from "../hooks/useFrecuente"
 import FrecuenteCard from './components/FrecuenteCard/FrecuenteCard'
 import Header from "./components/Header"
--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import Header from "./Header"
 import Helmet from "react-helmet"
 import Footer from "./Footer"
-import * as Global from "../context/GlobalContext"
+import * as Global from "../../context/GlobalContext"
 
 import '../../scss/main.scss'
 
